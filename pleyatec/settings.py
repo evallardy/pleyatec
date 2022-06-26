15da9ebea00cc0e3d082dd0b663edf10b901a481
enrique.vallardy.contractor <enrique.vallardy.contractor@bbva.com>
2022-06-26 18:25:47 -0500

    primer commit 46

--- pleyatec/settings.py
+++ pleyatec/settings.py
@@ -168,7 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
