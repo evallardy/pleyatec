f37237b9fd1a086b6212f5484ef8811ed7ef345a
enrique.vallardy.contractor <enrique.vallardy.contractor@bbva.com>
2022-06-26 17:25:28 -0500

    primer commit 42

--- pleyatec/settings.py
+++ pleyatec/settings.py
@@ -166,7 +166,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
