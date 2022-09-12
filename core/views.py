a9c3e9f3ee4798687a76d6642afc94dbfab2cd83
enrique.vallardy.contractor <enrique.vallardy.contractor@bbva.com>
2022-09-11 22:56:42 -0500

    v3.2.3 add User

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from bien.models import *
 from .forms import *
